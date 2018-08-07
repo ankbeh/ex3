fc2f7a64ded6c7616c72d0ad0f1c45c0b0696bb5
ankbeh <ankitbuzzid@gmail.com>
2018-08-07 13:08:57 +0530

    Nodemailer Works

--- routes/contact.js
+++ routes/contact.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var nodemailer = require("nodemailer");
+var useUser = require('./useUser.json');
 var router = express.Router();
 
 /* GET home page. */
@@ -8,14 +9,16 @@ router.get('/', function(req, res, next) {
 });
 router.post('/send', function(req, res, next){
   var tranporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    host: useUser.host,
+    port: useUser.port,
+    secure: useUser.secure,
     service: 'gmail',
     auth: {
-      user: 'bumbuzz555.1@gmail.com',
-      pass: '7289815803'
-    }
+      user: useUser.name,
+      pass: useUser.password
+    },
+    logger: true,
+    debug: true
   });
   var mailOptions = {
     form: 'Ankit <bumbuzz555.1@gmail.com>',
@@ -24,7 +27,7 @@ router.post('/send', function(req, res, next){
     text: 'You have a new submission with the following details...Name: '+ req.body.name + ' Email: '+ req.body.email + ' Message: '+ req.body.message,
     html: '<p>You have a new submission with the following details...</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
   };
-  tranporter.sendMail(mailOptions, function(error,infio){
+  tranporter.sendMail(mailOptions, function(error,info){
     if(error){
       console.log(error);
       res.redirect('/');
